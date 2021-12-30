e0bd49f07a8fe37ce850f64c4f9d6a5cffc5a90d
syrym-umbetov <umbetov.syrym@gmail.com>
2021-12-30 21:20:14 +0500

    Navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,21 @@
+import {
+  Navbar,
+  Welcome,
+  Footer,
+  Services,
+  Transactions,
+  Loader,
+} from './components';
 const App = () => {
   return (
-    <div className='App'>
-      <h1 class='text-3xl font-bold underline'>Hello world!</h1>
-      <p class='font-bold'>Suka</p>
+    <div className='min-h-screen'>
+      <div className='gradient-bg-welcome'>
+        <Navbar />
+        <Welcome />
+      </div>
+      <Services />
+      <Transactions />
+      <Footer />
     </div>
   );
 };
