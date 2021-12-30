04766b987d9f9173dde810c35105e0108321489e
syrym-umbetov <umbetov.syrym@gmail.com>
2021-12-30 18:47:56 +0500

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div className='App'>
+      <h1 class='text-3xl font-bold underline'>Hello world!</h1>
+      <p class='font-bold'>Suka</p>
+    </div>
+  );
+};
+
+export default App;
